b8fd48dd3734bcfd1b70bbc94602c045d1525021
Patryk Wojewoda <wojewodix@gmail.com>
2024-04-30 15:02:15 +0200

    Added logic to hide tabs in the top navigation bar and cards on the homepage if the user is not logged in. Created a carousel on the homepage with brief product descriptions when the user is not logged in.

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import Card from "react-bootstrap/Card";
 import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { LoginContext } from "./Components/Contexts/LoginContext";
+import Carousel from "react-bootstrap/Carousel";
+import Image from "react-bootstrap/Image";
 
 function App() {
   const { isLoggedIn, setIsLoggedIn } = useContext(LoginContext);
@@ -17,129 +19,239 @@ function App() {
   };
   return (
     <Container>
-      <Row>{isLoggedIn && <h1>Zalogowano</h1>}</Row>
-      <Row>
-        <div className="centered-content">
-          <Col>
-            <Card style={{ width: "18rem" }}>
-              <Card.Img
-                variant="top"
-                src="../src/Components/Pictures/ManagementPicture.jpg"
-                className="cardsLogo"
-              />
-              <Card.Body>
-                <Card.Title>Zarządzanie</Card.Title>
-                <Card.Text>
-                  Składanie nowych zamówień, przypisywanie maszyn, oraz
-                  zapisywanie przewidywanego czasu realizacji
-                </Card.Text>
-                <Button variant="primary">
-                  <Link to={"/Management"}>Idz do Zarządzania</Link>
-                </Button>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col>
-            <Card style={{ width: "18rem" }}>
-              <Card.Img
-                variant="top"
-                src="../src/Components/Pictures/ProductionPicture.jpg"
-                className="cardsLogo"
-              />
-              <Card.Body>
-                <Card.Title>Produkcja</Card.Title>
-                <Card.Text>
-                  Rozpoczynanie i kontrola czasu pracy. Zgłaszanie defektów
-                  towaru, lub ich braków.
-                </Card.Text>
-                <Button variant="primary">
-                  <Link to={"/Production"}>Idz do Produkcji</Link>
-                </Button>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col>
-            <Card style={{ width: "18rem" }}>
-              <Card.Img
-                variant="top"
-                src="../src/Components/Pictures/LogisticsPicture.jpg"
-                className="cardsLogo"
-              />
-              <Card.Body>
-                <Card.Title>Logistyka</Card.Title>
-                <Card.Text>
-                  Zgłaszanie rozpoczęcia/zakończenia transportu towaru.
-                  Zgłaszanie/odczyt aktualnej lokalizacji towaru.
-                </Card.Text>
-                <Button variant="primary">
-                  <Link to={"/Logistic"}>Idz do Logistyki</Link>
-                </Button>
-              </Card.Body>
-            </Card>
-          </Col>
-        </div>
-      </Row>
+      {isLoggedIn && (
+        <div>
+          <Row>
+            <div className="centered-content">
+              <Col>
+                <Card style={{ width: "18rem" }}>
+                  <Card.Img
+                    variant="top"
+                    src="../src/Components/Pictures/ManagementPicture.jpg"
+                    className="cardsLogo"
+                  />
+                  <Card.Body>
+                    <Card.Title>Zarządzanie</Card.Title>
+                    <Card.Text>
+                      Składanie nowych zamówień, przypisywanie maszyn, oraz
+                      zapisywanie przewidywanego czasu realizacji
+                    </Card.Text>
+                    <Button variant="primary">
+                      <Link to={"/Management"}>Idz do Zarządzania</Link>
+                    </Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col>
+                <Card style={{ width: "18rem" }}>
+                  <Card.Img
+                    variant="top"
+                    src="../src/Components/Pictures/ProductionPicture.jpg"
+                    className="cardsLogo"
+                  />
+                  <Card.Body>
+                    <Card.Title>Produkcja</Card.Title>
+                    <Card.Text>
+                      Rozpoczynanie i kontrola czasu pracy. Zgłaszanie defektów
+                      towaru, lub ich braków.
+                    </Card.Text>
+                    <Button variant="primary">
+                      <Link to={"/Production"}>Idz do Produkcji</Link>
+                    </Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col>
+                <Card style={{ width: "18rem" }}>
+                  <Card.Img
+                    variant="top"
+                    src="../src/Components/Pictures/LogisticsPicture.jpg"
+                    className="cardsLogo"
+                  />
+                  <Card.Body>
+                    <Card.Title>Logistyka</Card.Title>
+                    <Card.Text>
+                      Zgłaszanie rozpoczęcia/zakończenia transportu towaru.
+                      Zgłaszanie/odczyt aktualnej lokalizacji towaru.
+                    </Card.Text>
+                    <Button variant="primary">
+                      <Link to={"/Logistic"}>Idz do Logistyki</Link>
+                    </Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </div>
+          </Row>
 
-      <Row>
-        <div className="centered-content">
-          <Col>
-            <Card style={{ width: "18rem" }}>
-              <Card.Img
-                variant="top"
-                src="../src/Components/Pictures/PersonnelPicture.jpg"
-                className="cardsLogo"
-              />
-              <Card.Body>
-                <Card.Title>Kadry</Card.Title>
-                <Card.Text>
-                  Dodawanie, edycja oraz sprawdzanie listy pracowników.
-                </Card.Text>
-                <Button variant="primary">
-                  <Link to={"/Personnel"}>Idz do Kadr</Link>
-                </Button>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col>
-            <Card style={{ width: "18rem" }}>
-              <Card.Img
-                variant="top"
-                src="../src/Components/Pictures/FinancesPicture.jpg"
-                className="cardsLogo"
-              />
-              <Card.Body>
-                <Card.Title>Finanse</Card.Title>
-                <Card.Text>
-                  Obliczanie kosztów realizacji zlecenia. Sprawdzanie aktualnego
-                  stanu finansów. Zlecanie przelewów.
-                </Card.Text>
-                <Button variant="primary">
-                  <Link to={"/Finances"}>Idz do Finansów</Link>
-                </Button>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col>
-            <Card style={{ width: "18rem" }}>
-              <Card.Img
-                variant="top"
-                src="../src/Components/Pictures/RaportsPicture.jpg"
-                className="cardsLogo"
-              />
-              <Card.Body>
-                <Card.Title>Raporty i Analizy</Card.Title>
-                <Card.Text>
-                  Raporty miesieczne, kwartalne i roczne. Analiza zysków i
-                  strat.
-                </Card.Text>
-                <Button variant="primary">
-                  <Link to={"/Reports&Analytics"}>Idz do Finansów</Link>
-                </Button>
-              </Card.Body>
-            </Card>
-          </Col>
+          <Row>
+            <div className="centered-content">
+              <Col>
+                <Card style={{ width: "18rem" }}>
+                  <Card.Img
+                    variant="top"
+                    src="../src/Components/Pictures/PersonnelPicture.jpg"
+                    className="cardsLogo"
+                  />
+                  <Card.Body>
+                    <Card.Title>Kadry</Card.Title>
+                    <Card.Text>
+                      Dodawanie, edycja oraz sprawdzanie listy pracowników.
+                    </Card.Text>
+                    <Button variant="primary">
+                      <Link to={"/Personnel"}>Idz do Kadr</Link>
+                    </Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col>
+                <Card style={{ width: "18rem" }}>
+                  <Card.Img
+                    variant="top"
+                    src="../src/Components/Pictures/FinancesPicture.jpg"
+                    className="cardsLogo"
+                  />
+                  <Card.Body>
+                    <Card.Title>Finanse</Card.Title>
+                    <Card.Text>
+                      Obliczanie kosztów realizacji zlecenia. Sprawdzanie
+                      aktualnego stanu finansów. Zlecanie przelewów.
+                    </Card.Text>
+                    <Button variant="primary">
+                      <Link to={"/Finances"}>Idz do Finansów</Link>
+                    </Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col>
+                <Card style={{ width: "18rem" }}>
+                  <Card.Img
+                    variant="top"
+                    src="../src/Components/Pictures/RaportsPicture.jpg"
+                    className="cardsLogo"
+                  />
+                  <Card.Body>
+                    <Card.Title>Raporty i Analizy</Card.Title>
+                    <Card.Text>
+                      Raporty miesieczne, kwartalne i roczne. Analiza zysków i
+                      strat.
+                    </Card.Text>
+                    <Button variant="primary">
+                      <Link to={"/Reports&Analytics"}>Idz do Finansów</Link>
+                    </Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </div>
+          </Row>
         </div>
-      </Row>
+      )}
+
+      {!isLoggedIn && (
+        <div style={{ width: "100%", margin: 0, padding: 0 }}>
+          <header style={{ width: "100%", paddingLeft: 0 }}>
+            <div
+              className="p-3 text-center bg-image"
+              style={{
+                width: "100%",
+                height: 600,
+                backgroundSize: "cover",
+              }}
+            >
+              <div
+                className="mask"
+                style={{ backgroundColor: "rgba(0, 0, 0, 0.6)" }}
+              >
+                <div className="d-flex justify-content-center align-items-center h-100">
+                  <Carousel>
+                    <Carousel.Item>
+                      <Image
+                        src="src\Components\Pictures\CarouselPicrures\MainView.png"
+                        rounded
+                      />
+                      <Carousel.Caption
+                        style={{
+                          backgroundColor: "rgba(0, 0, 0, 0.75)",
+                          borderRadius: "15px",
+                          padding: "20px",
+                          border: "1px solid rgba(255, 255, 255, 0.5)",
+                        }}
+                      >
+                        <h3>Witaj w RidderPW</h3>
+                        <p>
+                          Poznaj możliwości systemu ERP stworzonego specjalnie
+                          na potrzeby twojej firmy!
+                        </p>
+                      </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                      <Image
+                        src="src\Components\Pictures\CarouselPicrures\ProductionView.png"
+                        rounded
+                      />
+                      <Carousel.Caption
+                        style={{
+                          backgroundColor: "rgba(0, 0, 0, 0.75)",
+                          borderRadius: "15px",
+                          padding: "20px",
+                          border: "1px solid rgba(255, 255, 255, 0.5)",
+                        }}
+                      >
+                        <h3>Dostosowany do Twoich Potrzeb</h3>
+                        <p>
+                          Ty mówisz czego oczekujesz od systemu ERP, a my to
+                          robimy!
+                        </p>
+                      </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                      <Image
+                        src="src\Components\Pictures\CarouselPicrures\ManagementView.png"
+                        rounded
+                      />
+                      <Carousel.Caption
+                        style={{
+                          backgroundColor: "rgba(0, 0, 0, 0.75)",
+                          borderRadius: "15px",
+                          padding: "20px",
+                          border: "1px solid rgba(255, 255, 255, 0.5)",
+                        }}
+                      >
+                        <h3>Prostota i Elegancja</h3>
+                        <p>
+                          Nasze intuicyjne rozwiązania ERP sprawiają że
+                          zarządzanie nawet najbardziej złożonymi procesami to
+                          czysta przyjemność!
+                        </p>
+                      </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                      <Image
+                        src="src\Components\Pictures\CarouselPicrures\technicalHelpView.png"
+                        rounded
+                      />
+                      <Carousel.Caption
+                        style={{
+                          backgroundColor: "rgba(0, 0, 0, 0.75)",
+                          borderRadius: "15px",
+                          padding: "20px",
+                          border: "1px solid rgba(255, 255, 255, 0.5)",
+                        }}
+                      >
+                        <h3>Wsparcie techniczne 24/7</h3>
+                        <p>
+                          Koniec z czekaniem na infolini! Nasi technicy są
+                          gotowi odpowiedzieć na panstwa pytania 24godziny na
+                          dobe 7 dni w tygodniu!
+                        </p>
+                      </Carousel.Caption>
+                    </Carousel.Item>
+                  </Carousel>
+                </div>
+              </div>
+            </div>
+          </header>
+        </div>
+      )}
     </Container>
   );
 }
--- src/Components/Leyouts/TopNavbar.jsx
+++ src/Components/Leyouts/TopNavbar.jsx
@@ -8,61 +8,110 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import { useContext } from "react";
+import { LoginContext } from "../Contexts/LoginContext";
 
 export const TopNavbar = () => {
+  const { isLoggedIn, setIsLoggedIn } = useContext(LoginContext);
+  const loginState = {
+    isLoggedIn,
+    setIsLoggedIn,
+  };
   return (
     <div>
-      <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
-        <Container>
-          <Navbar.Brand>
-            <Link to="/">
-              <Image
-                src="../src/Components/Pictures/RidderPW-logo.jpg"
-                className="logo"
-              />
-            </Link>
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link>
-                <Link to={"/Management"}>Zarządzanie</Link>
-              </Nav.Link>
-              <Nav.Link>
-                <Link to={"/Production"}>Produkcja</Link>
-              </Nav.Link>
-              <Nav.Link>
-                <Link to={"/Logistic"}>Logistyka</Link>
-              </Nav.Link>
-              <Nav.Link>
-                <Link to={"/Personnel"}>Kadry</Link>
-              </Nav.Link>
-              <Nav.Link>
-                <Link to={"/Finances"}>Finanse</Link>
-              </Nav.Link>
-              <Nav.Link>
-                <Link to={"/Reports&Analytics"}>Raporty i Analizy</Link>
-              </Nav.Link>
-            </Nav>
-            <Nav>
-              <Form inline>
-                <Row>
-                  <Col xs="auto">
-                    <Form.Control
-                      type="text"
-                      placeholder="Szukaj..."
-                      className=" mr-sm-2"
-                    />
-                  </Col>
-                  <Col xs="auto">
-                    <Button type="submit">Szukaj</Button>
-                  </Col>
-                </Row>
-              </Form>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+      {isLoggedIn && (
+        <div>
+          <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+            <Container>
+              <Navbar.Brand>
+                <Link to="/">
+                  <Image
+                    src="../src/Components/Pictures/RidderPW-logo.jpg"
+                    className="logo"
+                  />
+                </Link>
+              </Navbar.Brand>
+
+              <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+              <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="me-auto">
+                  <Nav.Link>
+                    <Link to={"/Management"}>Zarządzanie</Link>
+                  </Nav.Link>
+                  <Nav.Link>
+                    <Link to={"/Production"}>Produkcja</Link>
+                  </Nav.Link>
+                  <Nav.Link>
+                    <Link to={"/Logistic"}>Logistyka</Link>
+                  </Nav.Link>
+                  <Nav.Link>
+                    <Link to={"/Personnel"}>Kadry</Link>
+                  </Nav.Link>
+                  <Nav.Link>
+                    <Link to={"/Finances"}>Finanse</Link>
+                  </Nav.Link>
+                  <Nav.Link>
+                    <Link to={"/Reports&Analytics"}>Raporty i Analizy</Link>
+                  </Nav.Link>
+                </Nav>
+                <Nav>
+                  <Form inline>
+                    <Row>
+                      <Col xs="auto">
+                        <Form.Control
+                          type="text"
+                          placeholder="Szukaj..."
+                          className=" mr-sm-2"
+                        />
+                      </Col>
+                      <Col xs="auto">
+                        <Button type="submit">Szukaj</Button>
+                      </Col>
+                    </Row>
+                  </Form>
+                </Nav>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
+        </div>
+      )}
+
+      {!isLoggedIn && (
+        <div>
+          <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+            <Container>
+              <Navbar.Brand>
+                <Link to="/">
+                  <Image
+                    src="../src/Components/Pictures/RidderPW-logo.jpg"
+                    className="logo"
+                  />
+                </Link>
+              </Navbar.Brand>
+              <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+              <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="me-auto"></Nav>
+                <Nav>
+                  <Form inline>
+                    <Row>
+                      <Col xs="auto">
+                        <Form.Control
+                          type="text"
+                          placeholder="Szukaj..."
+                          className=" mr-sm-2"
+                        />
+                      </Col>
+                      <Col xs="auto">
+                        <Button type="submit">Szukaj</Button>
+                      </Col>
+                    </Row>
+                  </Form>
+                </Nav>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
+        </div>
+      )}
 
       <div>
         <Container fluid>
